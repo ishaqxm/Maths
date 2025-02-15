7c4df2df714607132d94b4d9be14710f96d1b4ef
Mohammed Ishaq <ishaqmi@proton.me>
2025-02-15 08:26:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("hey Git")
-print("hey Github")
+while True:
+	n=int(input('Enter a natural number: '))
+	for i in range(1,2*n,2):
+		print(i,end=' + ')
+		#
+	def odd(n):
+		if n == 1:
+			return 1
+		else:
+			return (n*2 - 1) + odd(n - 1)
+	print('\b\b=',odd(n),f'= ({n})²\n')
